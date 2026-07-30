16bf67e75cdf775c07041bc0efe9c05ed2e1dec2
lucasberenger <thegreatberenger@gmail.com>
2026-07-30 01:21:07 -0300

    create staging sql scripts and add them to container volumes

---
+++ sql/02_create_staging.sql
@@ -0,0 +1,48 @@
+CREATE TABLE staging.customers (
+    customer_id TEXT,
+    customer_unique_id TEXT,
+    customer_zip_code_prefix INTEGER,
+    customer_city TEXT,
+    customer_state CHAR(2)
+);
+
+CREATE TABLE staging.orders (
+    order_id TEXT,
+    customer_id TEXT,
+    order_status TEXT,
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP
+);
+
+CREATE TABLE staging.order_items (
+    order_id TEXT,
+    order_item_id INTEGER,
+    product_id TEXT,
+    seller_id TEXT,
+    shipping_limit_date TIMESTAMP,
+    price NUMERIC(10,2),
+    freight_value NUMERIC(10,2)
+);
+
+CREATE TABLE staging.products (
+    product_id TEXT,
+    product_category_name TEXT,
+    product_name_length INTEGER,
+    product_description_length INTEGER,
+    product_photos_qty INTEGER,
+    product_weight_g INTEGER,
+    product_length_cm INTEGER,
+    product_height_cm INTEGER,
+    product_width_cm INTEGER
+);
+
+CREATE TABLE staging.payments (
+    order_id TEXT,
+    payment_sequencial INTEGER,
+    payment_type TEXT,
+    payment_installments INTEGER,
+    payment_value NUMERIC(10,2)
+);
